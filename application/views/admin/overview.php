b8e7d171a6389460290a14456b838c42913c1b99
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-12-01 12:46:26 +0700

    first commit

---
+++ application/views/admin/overview.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php $this->load->view("admin/_partials/head.php") ?>
+</head>
+
+<body id="page-top">
+
+	<?php $this->load->view("admin/_partials/navbar.php") ?>
+
+	<div id="wrapper">
+
+		<?php $this->load->view("admin/_partials/sidebar.php") ?>
+
+		<div id="content-wrapper">
+
+			<div class="container">
+				<h1>Selamat Datang Di Website Toko Buah</h1>
+			</div>
+		</div>
+
+
+
+		<?php $this->load->view("admin/_partials/scrolltop.php") ?>
+		<?php $this->load->view("admin/_partials/modal.php") ?>
+		<?php $this->load->view("admin/_partials/js.php") ?>
+
+</body>
+
+</html>
