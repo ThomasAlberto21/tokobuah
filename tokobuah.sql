9e010a1310a71f240ab2e8454e5070894c4aa2d1
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-12-05 12:53:50 +0700

    FINISH!!!!!!!!!!!!

--- tokobuah.sql
+++ tokobuah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 05, 2023 at 04:25 AM
+-- Generation Time: Dec 05, 2023 at 06:53 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.2.25
 
@@ -41,8 +41,50 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `name`, `price`, `image`, `description`) VALUES
-('656e96b9e334e', 'Test', 12, 'default.jpg', 'test'),
-('656e9786498d3', 'test23', 123, 'default.jpg', 'test');
+('656e9786498d3', 'test23', 1230000, 'default.jpg', 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nik` varchar(25) DEFAULT NULL,
+  `nama_lengkap` varchar(100) DEFAULT NULL,
+  `jenis_kelamin` enum('Laki-laki','Perempuan') DEFAULT NULL,
+  `tanggal_lahir` date DEFAULT NULL,
+  `alamat_lengkap` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `nik`, `nama_lengkap`, `jenis_kelamin`, `tanggal_lahir`, `alamat_lengkap`) VALUES
+(656, '94384324', 'test', 'Laki-laki', '2023-12-05', 'test'),
+(658, '239214912', 'test', 'Laki-laki', '2023-12-05', 'test2434');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=659;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
